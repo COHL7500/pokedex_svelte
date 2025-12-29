111f3bbae4a5503c4cc38b2f5f5abcf8be42183a
Bastjan Rosgaard Sejberg <cohl7500@gmail.com>
2025-12-30 00:06:36 +0100

    feat: implement pokemon card view with detailed fetching

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,11 @@
 import type { Pokemon } from '$lib/types';
-import { fetchPokemonDetail, toPokemon } from '$lib/api/api';
-import type { PokeAPIResponse } from '$lib/api/types';
-import { API_BASE_URL } from '$lib/api/constants';
+import { fetchPokemonDetail, type PokeAPIResponse, toPokemon } from '$lib/api';
 
 export const load = async ({ fetch, url }) => {
 	const limitparam = url.searchParams.get('limit');
 	const limit = limitparam ? Math.max(1, Number(limitparam ?? '10')) : 10;
 
-	const res = await fetch(`${API_BASE_URL}/pokemon?limit=${limit}`);
+	const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
 
 	if (!res.ok) {
 		throw new Error('Failed to fetch Pokémon list');
@@ -16,9 +14,7 @@ export const load = async ({ fetch, url }) => {
 	const json = (await res.json()) as PokeAPIResponse;
 
 	const pokemons: Pokemon[] = await Promise.all(
-		json.results.map(async (pokemon) =>
-			toPokemon(await fetchPokemonDetail(pokemon.url))
-		)
+		json.results.map(async (pokemon) => toPokemon(await fetchPokemonDetail(pokemon.url)))
 	);
 
 	return { pokemons };
