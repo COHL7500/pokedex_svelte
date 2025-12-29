775d13f4a1d26fbeaf05ed0bd969488101b2f1fb
Bastjan Rosgaard Sejberg <cohl7500@gmail.com>
2025-12-29 10:31:33 +0100

    feat: add new font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,15 @@ let { children } = $props();
 </script>
 
 <svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
+
+<style>
+	:global(html, body) {
+		font-family: Exo, sans-serif;
+	}
+</style>
