9f3fc48260547227c50aa5012bca2bea63f4f8d8
Bastjan Rosgaard Sejberg <cohl7500@gmail.com>
2026-01-02 23:28:54 +0100

    feat: implement searching for specific pokemon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,36 +5,60 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
 	import { SvelteURLSearchParams } from 'svelte/reactivity';
+	import { buildUrl } from '$lib/utils';
 
 	const { data } = $props();
-	let query = $state('');
+	const sp = new SvelteURLSearchParams(page.url.searchParams);
 
-	const pokemons = $derived.by(() => {
-		if (!query) return data.pokemons;
-
-		const result = data.pokemons.filter((pokemon) =>
-			pokemon.name.toLowerCase().includes(query.toLowerCase())
-		);
-
-		return result;
-	});
+	let query = $state(sp.get('q') ?? '');
 
 	const meta = $derived(data.meta);
 
-	const navigate = async (offset: number | null) => {
+	const syncQueryURL = () => {
+		if (query) {
+			sp.set('q', query);
+			sp.set('offset', '0');
+		} else {
+			sp.delete('q');
+		}
+
+		// eslint-disable-next-line svelte/no-navigation-without-resolve
+		goto(buildUrl(sp), {
+			replaceState: true,
+			keepFocus: true,
+			noScroll: true
+		});
+	};
+
+	$effect(() => {
+		syncQueryURL();
+	});
+
+	const navigate = (offset: number | null) => {
 		if (offset == null) return;
 
-		const sp = new SvelteURLSearchParams(page.url.searchParams);
 		sp.set('limit', String(meta.limit));
 		sp.set('offset', String(offset));
 
 		// eslint-disable-next-line svelte/no-navigation-without-resolve
-		await goto(`?${sp.toString()}`);
+		goto(buildUrl(sp));
 	};
 </script>
 
 <main class="container">
 	<h1>Pokémons</h1>
+
+	<div class="search-container">
+		<SearchInput bind:query></SearchInput>
+	</div>
+
+	<Grid>
+		{#each data.pokemons as pokemon (pokemon.name)}
+			<li>
+				<PokemonCard {pokemon} />
+			</li>
+		{/each}
+	</Grid>
 
 	<nav class="pagination">
 		<button
@@ -51,24 +75,40 @@
 		</button>
 		<span>Page {meta.currPage} of {meta.totalPages}</span>
 	</nav>
-
-	<div class="search-container">
-		<SearchInput bind:query></SearchInput>
-	</div>
-
-	<Grid>
-		{#each pokemons as pokemon (pokemon.name)}
-			<li>
-				<PokemonCard {pokemon} />
-			</li>
-		{/each}
-	</Grid>
 </main>
 
 <style>
 	.search-container {
 		margin-bottom: 1rem;
 		max-width: 300px;
+	}
+
+	.pagination {
+		position: fixed;
+		left: 50%;
+		bottom: 1rem;
+		transform: translateX(-50%);
+		z-index: 1000;
+
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+
+		padding: 0.75rem 1rem;
+		border-radius: 10% / 70%;
+		background: rgba(255, 255, 255);
+		box-shadow: 0 8px 24px rgba(0, 0, 0, 0.15);
+
+		button {
+			padding: 0.5rem 1rem;
+			font-size: 1rem;
+			border: none;
+			border-radius: 5px;
+			background-color: #02346f;
+			color: white;
+			cursor: pointer;
+			transition: background-color 0.3s ease;
+		}
 	}
 
 	.container {
